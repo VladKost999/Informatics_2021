6143453d5aed7ee6c86295111708f7779a7833d3
unknown <Russia7780030513@gmail.com>
2021-10-04 11:22:57 +0300

    Файл питонщика(WARNING)

--- first.py
+++ first.py
@@ -1,4 +1,16 @@
+bool_sample=True
+
 a=2
 b=3
 c=a+b
 print('Hello world')
+
+age = 5
+if age <=7:
+    print('Doshkolnik')
+if age >7 and age<=18:
+    print('Shkolnik') 
+if age>18 and age<=25:
+    print('Student')
+if age >26 :
+    print('Rabotyaga')
